8ded7ae19e0eab30d2541773711d845faa46b69f
Rosa <xenco34@gmail.com>
2023-02-27 11:00:05 +0100

    2oEjercicio

---
+++ 2oEjercicio.js
@@ -0,0 +1,13 @@
+const MyName="Rosa";
+const age=40;
+const BirthDate=new Date("February 16 1983");
+
+const FavBook={
+    title:"PADRE RICO, PADRE POBRE",
+    author:"Robert Kiyosaki, Sharon Lechter",
+    launch:1997,
+    url:"https://www.amazon.es/Padre-Rico-padre-pobre-Advisors/dp/160396181X"
+}
+
+const personalInf=[MyName,age,false,BirthDate,FavBook];
+console.log(personalInf);
